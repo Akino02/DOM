2a631acf3018926990edf98f8755607b592bd3a7
michal <romster2012@seznam.cz>
2022-10-20 09:19:37 +0200

    upgrade_1.2

--- res/js/main.js
+++ res/js/main.js
@@ -1,3 +1,11 @@
-const userinput = document.getElementById("userinput")
+const userinputX = document.getElementById("userinputX")
+const userinputY = document.getElementById("userinputY")
 const send = document.getElementById("send")
 const content = document.getElementById("content")
+
+send.onclick = () => {
+    const userinputXValue = userinputX.value;
+    const userinputYValue = userinputY.value;
+    content.style.left = `${userinputXValue}px`;
+    content.style.top = `${userinputYValue}px`;
+}
